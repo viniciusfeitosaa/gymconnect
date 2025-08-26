2fb049432911471ae75bafecd764e197147f6775
Vinícius Feitosa <viniciusfeitosa@protonmail.com>
2025-08-26 18:48:36 -0300

    Fix: remove duplicate netlify.toml and add package.json for functions

---
+++ client/netlify/functions/test.js
@@ -0,0 +1,18 @@
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      message: 'Test function working!',
+      timestamp: new Date().toISOString(),
+      event: {
+        httpMethod: event.httpMethod,
+        path: event.path,
+        headers: event.headers
+      }
+    })
+  };
+};
